534e980b977c56949b6078f16a8b5775bc0a971f
bushra223 <bushra_uet@hotmail.com>
2024-05-09 10:58:30 +0500

    chalk added

--- Assignment3/calculator.ts
+++ Assignment3/calculator.ts
@@ -4,6 +4,7 @@ import { subtraction } from "./subtraction.js";
 import { division } from "./division.js";
 import { multiplication } from "./multiplication.js";
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 let flag = false;
 while (flag == false) {
@@ -11,33 +12,50 @@ while (flag == false) {
 		{
 			name: "number1",
 			type: "number",
-			message: "Enter first number:",
+			message: chalk.bgHex("#98eb34").bold("Enter first number:"),
 		},
 		{
 			name: "number2",
 			type: "number",
-			message: "Enter second number:",
+			message: chalk.bold.bgHex("#34ad77").bold("Enter second number:"),
 		},
 		{
 			name: "operations",
 			type: "list",
-			message: "Select operation:",
+			message: chalk.bold.bgHex("#4b4982").bold.underline("Select operation:"),
 			choices: ["Addition", "Subtraction", "Multiplication", "Division"],
 		},
 	]);
 	if (answer.operations == "Addition") {
-		console.log("Answer:" + addition(answer.number1, answer.number2));
+		console.log(
+			chalk.greenBright.bgBlueBright.bold(
+				"\t\t\tAnswer:" + addition(answer.number1, answer.number2).toFixed(2)
+			)
+		);
 	} else if (answer.operations == "Subtraction") {
-		console.log("Answer:" + subtraction(answer.number1, answer.number2));
+		console.log(
+			chalk.greenBright.bgBlueBright.bold(
+				'\t\t"Answer:' + subtraction(answer.number1, answer.number2).toFixed(2)
+			)
+		);
 	} else if (answer.operations == "Multiplication") {
-		console.log("Answer:" + multiplication(answer.number1, answer.number2));
+		console.log(
+			chalk.greenBright.bgBlueBright.bold(
+				'\t\t"Answer:' +
+					multiplication(answer.number1, answer.number2).toFixed(2)
+			)
+		);
 	} else if (answer.operations == "Division") {
-		console.log("Answer:" + division(answer.number1, answer.number2));
+		console.log(
+			chalk.greenBright.bgBlueBright.bold(
+				'\t\t"Answer:' + division(answer.number1, answer.number2).toFixed(2)
+			)
+		);
 	}
 	let runAgain = await inquirer.prompt([
 		{
 			name: "continue",
-			message: "Do you want to continue?",
+			message: chalk.blue.bold("Do you want to continue?"),
 			type: "confirm",
 		},
 	]);
